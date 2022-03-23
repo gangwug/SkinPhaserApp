2332cc9013fb863138d6d91b3c42bdc0cfb7aadc
Gang Wu <wggucas@gmail.com>
2022-03-23 13:58:03 -0400

    enlarge case

--- ui.R
+++ ui.R
@@ -7,32 +7,32 @@ shinyUI(fluidPage(
     sidebarPanel(
       fluidRow(
         column(6,
-               fileInput('file1', label=h5('Choose data file'),
+               fileInput('file1', label=h3('Choose data file'),
                          accept=c('text/csv', 
                                   'text/comma-separated-values,text/plain', '.csv')) ),
         column(6,
-               radioButtons('fstyle', label=h5('File style'),
+               radioButtons('fstyle', label=h3('File style'),
                             choices=c("csv"='csv', "txt"='txt'),
                             selected='csv', inline=FALSE) )
       ),
      fluidRow(
        column(12,
-              radioButtons('markerGenesReminder', label=h5('How many marker genes were measured in epidermal samples?'),
+              radioButtons('markerGenesReminder', label=h3('How many marker genes were measured in epidermal samples?'),
                            choices=c("all 12 marker genes"='all', 
                                      "7 marker genes (ARNTL, NR1D1, CIART, PER1/2/3, DBP) or more"='partial'),
                            selected='all', inline=FALSE) )
       ),
      fluidRow(
        column(12,
-              textInput("normGenes", label=h5("Internal control genes used for normalization"), value="GPKOW, BMS1, ANKFY1") )
+              textInput("normGenes", label=h3("Internal control genes used for normalization"), value="GPKOW, BMS1, ANKFY1") )
      ),
      br(),
      br(),
      fluidRow(
        column(3,
-              actionButton("update", label=h5("Run")) ),
+              actionButton("update", label=h3("Run")) ),
        column(3,
-              downloadButton('downloadData', label='Download' ) )
+              downloadButton('downloadData', label=h3('Download') ) )
      )
     ),
     
